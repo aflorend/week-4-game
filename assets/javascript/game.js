c16a22aba86945aea531a2829f9d459f8e5e9984
Angelo <aflorend@gmail.com>
2017-03-01 21:01:47 -0600

    100% full functionality. Ready for themes and styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,118 @@
-//Setting variables.
-var winCount = 0;
+	var winCount = 0;
 
-var lossCount = 0;
+	var lossCount = 0;
 
-var roundCount = 1;
+	var roundCount = 1;
 
-var fightCount = 0;
+	var fightCount = 0;
 
-var action = 0;
+	var fightReady = false;
 
-$('.attackBtn').css('visibility', 'visible');
+	var action = 0;
 
-$('.resetBtn').css('visibility', 'hidden');
+function reset() {
 
-//Setting character objects.
-var char1 = {
-	healthPoints: 100,
+	//Resetting Variables to inital values.
+	winCount = 0;
 
-	attackPoints: 5,
+	lossCount = 0;
 
-	counterPoints: 5,
+	roundCount = 1;
 
-	image: '.../images/1.jpg',
+	fightCount = 0;
 
-	name: '1'
+	fightReady = false;
+
+	action = 0;
+
+
+	//Empties last game's character divs.
+	$('.charactersRow, .attackerBox, .defenderBox').empty();
+	
+	//Setting character objects.
+	var characters = [
+	{
+		healthPoints: 100,
+
+		attackPoints: 5,
+
+		counterPoints: 5,
+
+		image: '.../images/1.jpg',
+
+		name: 'char0',
+
+		title: 'char0'
+	},
+
+	{
+		healthPoints: 100,
+
+		attackPoints: 5,
+
+		counterPoints: 5,
+
+		image: '.../images/2.jpg',
+
+		name: 'char1',
+
+		title: 'char1'
+	},
+
+	{
+		healthPoints: 100,
+
+		attackPoints: 5,
+
+		counterPoints: 5,
+
+		image: '.../images/3.jpg',
+
+		name: 'char2',
+
+		title: 'char2'
+	},
+
+	{
+		healthPoints: 100,
+
+		attackPoints: 5,
+
+		counterPoints: 5,
+
+		image: '.../images/4.jpg',
+
+		name: 'char3',
+
+		title: 'char3'
+	}];
+
+	//Creating character divs for character objects.
+	for (i=0; i < characters.length; i++) {
+		var newDiv = $('<div>');
+		newDiv.appendTo('.charactersRow').addClass('col-md-3 charBox');
+		newDiv.append('<div class="charDiv" id="char' + i + '"></div>')
+		$("#char" + i).data(characters[i]).text("Health Points :" + characters[i].healthPoints);
+	};
+
+	//Associating character data with their respective elements.
+	// $('#char1').data(char1);
+	// $('#char2').data(char2);
+	// $('#char3').data(char3);
+	// $('#char4').data(char4);
+
+	$('.attackBtn').css('visibility', 'hidden');
+
+	$('.resetBtn').css('visibility', 'hidden');
+
+	game();
 };
 
-var char2 = {
-	healthPoints: 150,
+//Starting game with reset values.
+reset();
 
-	attackPoints: 5,
-
-	counterPoints: 5,
-
-	image: '.../images/2.jpg',
-
-	name: '2'
-};
-
-var char3 = {
-	healthPoints: 100,
-
-	attackPoints: 5,
-
-	counterPoints: 5,
-
-	image: '.../images/3.jpg',
-
-	name: '3'
-};
-
-var char4 = {
-	healthPoints: 100,
-
-	attackPoints: 5,
-
-	counterPoints: 5,
-
-	image: '.../images/4.jpg',
-
-	name: '4'
-};
-
-//Associating character data with their respective elements.
-$('#char1').data(char1);
-$('#char2').data(char2);
-$('#char3').data(char3);
-$('#char4').data(char4);
-
+function game() {
+$('#info').text('Select your character!');
 
 //Clicking character sets it as user's attacker, 2nd click sets as 1st defender.
 $('.charDiv').on('click', function(){
@@ -76,22 +121,23 @@ $('.charDiv').on('click', function(){
 	};
 
 	if (action === 0) {
-		$(this).addClass('attacker');
-		console.log('attacker set');
+		$(this).addClass('attacker').appendTo('.attackerBox');
+		$('#info').text('Select your enemy!');
 		action++;
-
-		$('.charDiv').on('click', function(){
-			if ( $(this).hasClass('attacker') || $(this).hasClass('defender') ) {
-				return false;
-			};
-			if (action === 1) {
-				$(this).addClass('defender');
-				console.log('defender set');
-				action++
-				fightReady = true;
-			}
-		});
 	};
+});
+
+$('.charDiv').on('click', function(){
+	if ( $(this).hasClass('attacker') || $(this).hasClass('defender') ) {
+		return false;
+	};
+	if (action === 1) {
+		$(this).addClass('defender').appendTo('.defenderBox');
+		$('.attackBtn').css('visibility', 'visible');
+		$('#info').text('Attack!');
+		action++
+		fightReady = true;
+	}
 });
 
 //Attack button actions
@@ -104,25 +150,27 @@ $('.attackBtn').on('click', function() {
 		var defCP = $('.defender').data('counterPoints');
 		var defHP = $('.defender').data('healthPoints');
 
-		$('.defender').data('healthPoints', (defHP - (atkAP * roundCount)));
-		$('.attacker').data('healthPoints', (atkHP - defCP));
+		//Fight and round calculations.
+		var attackDamage = atkAP * roundCount;
+		$('.defender').data('healthPoints', (defHP - attackDamage));
+		$('.attacker').data('healthPoints', atkHP - defCP);
 		roundCount++;
-		
-		console.log(roundCount);
-		console.log('Attacker HP: ' + $('.attacker').data('healthPoints'));
-		console.log('Defender HP: ' + $('.defender').data('healthPoints'));
 
-	}
+		//Printing damage and updated health info.
+		$('.defender').text('Health Points :' + $('.defender').data('healthPoints'));
+		$('.attacker').text('Health Points :' + $('.attacker').data('healthPoints'));
+		$('#info').text('You attacked ' + $('.defender').data('title') + ' for ' + attackDamage + ' damage. ' + $('.defender').data('title') + ' counter attacked you for ' + defCP + ' damage.' );
+	};
 
 	//Removes defender if they have no HP left and waits for user to select new defender.
 	if ($('.defender').data('healthPoints') <= 0) {
-		$('.defender').css('display','none');
-		$('.defender').removeClass('defender');
-		console.log('removed defender');
+		$('.defender').remove();
+		$('#info').text('You defeated the enemy! Move on to the next round and select another enemy!');
+		$('.attackBtn').css('visibility', 'hidden');
 		fightReady = false;
 		action = 1;
 		fightCount++;
-	}
+	};
 
 	//User is dead if HP is depleted
 	if ($('.attacker').data('healthPoints') <= 0) {
@@ -134,36 +182,14 @@ $('.attackBtn').on('click', function() {
 
 	//If user goes through all 3 rounds, they win. Allows for user to reset.
 	if (fightCount === 3) {
-		console.log('You Win');
+		$('#info').text('You defeated all enemies! Do you want to try again?');
 		winCount++;
 		$('.attackBtn').css('visibility', 'hidden');
 		$('.resetBtn').css('visibility', 'visible');
 	}
 });
 
-//Reseting game and values.
-function reset() {
-	roundCount = 1;
-
-	fightCount = 0;
-
-	action = 0;
-
-	//Reintroduces hidden elements.
-	$('.attackBtn').css('visibility', 'visible');
-
-	$('.resetBtn').css('visibility', 'hidden');
-
-	$('.charDiv').css('display', 'initial');
-
-	$('.attacker').removeClass('attacker');
-	
-	//Resets character data.
-	$('#char1').data(char1);
-	$('#char2').data(char2);
-	$('#char3').data(char3);
-	$('#char4').data(char4);
-}
+};
 
 //Reset Button
 $('.resetBtn').on('click', function(){
